a1882421f7a4c0535e8ed780765a122c6656fbf4
musale <musale@focusmobile.co>
2017-08-01 13:36:31 +0300

    Update(postgres): add connection to local postgres db

--- golang-postgres/main.go
+++ golang-postgres/main.go
@@ -3,15 +3,43 @@ package main
 import (
 	"net/http"
 
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/musale/going-somewhere/golang-postgres/lib"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	log.WithFields(log.Fields{
-		"animal": "peng",
-	}).Info("A peng appears")
+	err := godotenv.Load()
+	if err != nil {
+		log.WithFields(log.Fields{"error": err}).Info("Error loading .env file")
+	}
+	DbUser := os.Getenv("DB_USER")
+	DbPassword := os.Getenv("DB_PASSWORD")
+	DbName := os.Getenv("DB_NAME")
+
+	dbinfo := fmt.Sprintf("postgres://%s:%s@localhost/%s?sslmode=disable",
+		DbUser, DbPassword, DbName)
+	db, err := sql.Open("postgres", dbinfo)
+	checkErr(err)
+	defer db.Close()
+
+	log.WithFields(log.Fields{"inserting fields": true}).Info("Inserting fields")
+	var lastInsertID int
+	err = db.QueryRow("INSERT INTO userinfo(username,departname,created) VALUES($1,$2,$3) returning uid;", "astaxie", "研发部门", "2012-12-09").Scan(&lastInsertID)
+	checkErr(err)
+	log.WithFields(log.Fields{"lastInsertID": lastInsertID}).Info("Last insert id")
 
 	http.HandleFunc("/", lib.MessagesPage)
 	http.ListenAndServe(":5505", nil)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Error(err)
+	}
 }
