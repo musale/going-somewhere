859f80fed567a444c865cf469da84a5b8d8c5003
Martin Musale <martinmshale@gmail.com>
2018-03-26 21:57:20 +0300

    Use a constant name for the prefix greeting

--- testing/hello_test.go
+++ testing/hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	got := Hello("Thiel")
-	want := "Hello Thiel"
+	want := "Hello, Thiel"
 
 	if got != want {
 		t.Errorf("got '%s' want '%s'", got, want)
--- testing/main.go
+++ testing/main.go
@@ -2,11 +2,13 @@ package main
 
 import "fmt"
 
+const helloPrefix = "Hello, "
+
 func main() {
 	fmt.Println(Hello("Kim"))
 }
 
 // Hello returns a string
 func Hello(name string) string {
-	return "Hello " + name
+	return helloPrefix + name
 }
