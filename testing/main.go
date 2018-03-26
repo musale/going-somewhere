e4f2065a68dc062f1017d5c8a137f25682089bc4
Martin Musale <martinmshale@gmail.com>
2018-03-26 22:28:42 +0300

    Refactor prefix code into its own function

--- testing/main.go
+++ testing/main.go
@@ -17,12 +17,17 @@ func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	prefix := englishPrefix
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case spanish:
 		prefix = spanishPrefix
 	case french:
 		prefix = frenchPrefix
+	default:
+		prefix = englishPrefix
 	}
-	return prefix + name
+	return
 }
