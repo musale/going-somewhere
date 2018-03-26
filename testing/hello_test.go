f4861cb7a44957e281d30f5fca3af22b121239bf
Martin Musale <martinmshale@gmail.com>
2018-03-26 22:14:08 +0300

    Refactor tests into subtests and refactor assertion

--- testing/hello_test.go
+++ testing/hello_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Thiel")
-	want := "Hello, Thiel"
-
-	if got != want {
-		t.Errorf("got '%s' want '%s'", got, want)
+	assertCorrect := func(t *testing.T, got, want string) {
+		if got != want {
+			t.Errorf("%s is not the same as %s", got, want)
+		}
 	}
+	t.Run("print name passed in args", func(t *testing.T) {
+		got := Hello("Thiel")
+		want := "Hello, Thiel"
+		assertCorrect(t, got, want)
+	})
+
+	t.Run("print default when args is empty string", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrect(t, got, want)
+
+	})
+
 }
