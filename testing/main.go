38ee1d5e25b635b52890234c2a21de45d0730b8f
Martin Musale <martinmshale@gmail.com>
2018-03-26 22:22:53 +0300

    Handle different languages

--- testing/main.go
+++ testing/main.go
@@ -2,16 +2,26 @@ package main
 
 import "fmt"
 
-const helloPrefix = "Hello, "
+const englishPrefix = "Hello, "
+const spanish = "Spanish"
+const spanishPrefix = "Hola, "
+const french = "French"
+const frenchPrefix = "Bonjour, "
 
 func main() {
-	fmt.Println(Hello("Kim"))
+	fmt.Println(Hello("Kim", "English"))
 }
 
 // Hello returns a string
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return helloPrefix + name
+	if language == spanish {
+		return spanishPrefix + name
+	}
+	if language == french {
+		return frenchPrefix + name
+	}
+	return englishPrefix + name
 }
