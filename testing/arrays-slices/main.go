21c31931151e947b75dbe3f0a9820c7a97ec9ddb
Martin Musale <martinmshale@gmail.com>
2018-03-30 21:54:16 +0300

    Add test for arrays and slices

---
+++ testing/arrays-slices/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	sum := Sum([5]int{1, 2, 3, 4, 5})
+	fmt.Println(sum)
+}
+
+func Sum(numbers [5]int) int {
+	sum := 0
+	for i := 0; i < 5; i++ {
+		sum += numbers[i]
+	}
+	return sum
+}
