93452dc1ef7adbef59d0f8474813ee6b8c32c77d
Martin Musale <martinmshale@gmail.com>
2018-03-30 21:56:41 +0300

    Use range to loop through the numbers

--- testing/arrays-slices/main.go
+++ testing/arrays-slices/main.go
@@ -10,8 +10,8 @@ func main() {
 // Sum adds numbers in an array
 func Sum(numbers [5]int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
 }
