ebba4a37e3484e6006b4305f0348ab802a70f5ff
Martin Musale <martinmshale@gmail.com>
2018-03-30 22:06:27 +0300

    Change from using arrays to using slices

--- testing/arrays-slices/main.go
+++ testing/arrays-slices/main.go
@@ -1,14 +1,11 @@
 package main
 
-import "fmt"
-
 func main() {
-	sum := Sum([5]int{1, 2, 3, 4, 5})
-	fmt.Println(sum)
+	Sum([]int{1, 2, 3, 4, 5})
 }
 
 // Sum adds numbers in an array
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
