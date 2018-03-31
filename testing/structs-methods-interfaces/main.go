8766083706c83e317882b7c584e811a32ac1aeca
Martin Musale <martinmshale@gmail.com>
2018-03-31 12:01:13 +0300

    Add the Perimeter function with temporary spec

---
+++ testing/structs-methods-interfaces/main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	Perimeter(30.4, 20.1)
+}
+
+// Perimeter gives the perimeter of a 4 sided
+func Perimeter(width, height float64) float64 {
+	return width + height
+}
