bc5b0c3ae5e9b95e7dc0b7708b05bafb62ef95fa
Martin Musale <martinmshale@gmail.com>
2018-04-01 22:53:14 +0300

    Use more than 2 decimal places in Circle area assertion

--- testing/structs-methods-interfaces/shapes_test.go
+++ testing/structs-methods-interfaces/shapes_test.go
@@ -7,26 +7,26 @@ func TestPerimeter(t *testing.T) {
 	perimeter := Perimeter(rectangle)
 	expected := 142.6
 	if perimeter != expected {
-		t.Errorf("expected %.2f but found %.2f", expected, perimeter)
+		t.Errorf("expected %.2f but got %.2f", expected, perimeter)
 	}
 }
 
 func TestArea(t *testing.T) {
 	t.Run("rectangles area", func(t *testing.T) {
 		rectangle := Rectangle{12.0, 3.0}
-		area := Area(rectangle)
-		expected := 36.0
+		area := rectangle.Area()
+		expected := 72.0
 		if area != expected {
-			t.Errorf("expected %.2f but found %.2f", expected, area)
+			t.Errorf("expected %.2f but got %.2f", expected, area)
 		}
 	})
 
 	t.Run("circles area", func(t *testing.T) {
 		circle := Circle{10}
-		area := Area(circle)
-		expected := 314.16
+		area := circle.Area()
+		expected := 314.1592653589793
 		if area != expected {
-			t.Errorf("expected %.2f but found %.2f", expected, area)
+			t.Errorf("expected %f but got %f", expected, area)
 		}
 	})
 }
