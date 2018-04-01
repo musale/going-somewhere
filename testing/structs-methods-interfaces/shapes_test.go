01f44fc31134bd9f3e49fb65386d379bd5f16edf
Martin Musale <martinmshale@gmail.com>
2018-04-01 22:41:27 +0300

    Add tests for circle

--- testing/structs-methods-interfaces/shapes_test.go
+++ testing/structs-methods-interfaces/shapes_test.go
@@ -12,10 +12,21 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 3.0}
-	area := Area(rectangle)
-	expected := 36.0
-	if area != expected {
-		t.Errorf("expected %.2f but found %.2f", expected, area)
-	}
+	t.Run("rectangles area", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 3.0}
+		area := Area(rectangle)
+		expected := 36.0
+		if area != expected {
+			t.Errorf("expected %.2f but found %.2f", expected, area)
+		}
+	})
+
+	t.Run("circles area", func(t *testing.T) {
+		circle := Circle{10}
+		area := Area(circle)
+		expected := 314.16
+		if area != expected {
+			t.Errorf("expected %.2f but found %.2f", expected, area)
+		}
+	})
 }
