73be577b78dd1390f913c7db52de39e40cd2f089
Martin Musale <martinmshale@gmail.com>
2018-04-02 23:18:17 +0300

    Refactor code to have more descriptive names and error messsages

--- testing/structs-methods-interfaces/shapes_test.go
+++ testing/structs-methods-interfaces/shapes_test.go
@@ -13,18 +13,22 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, 314.1592653589793},
-		{Triangle{12, 6}, 36.0},
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
 	for _, areaTest := range areaTests {
-		got := areaTest.shape.Area()
-		if got != areaTest.want {
-			t.Errorf("expected %.2f but got %.2f", areaTest.want, got)
-		}
+		t.Run(areaTest.name, func(t *testing.T) {
+			got := areaTest.shape.Area()
+			if got != areaTest.hasArea {
+				t.Errorf("%#v expected %.2f but got %.2f", areaTest.name, areaTest.hasArea, got)
+			}
+		})
+
 	}
 }
