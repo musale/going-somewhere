f14e95ee87d8ea43217f7160b1ac0bc74550e058
Martin Musale <martinmshale@gmail.com>
2018-04-03 19:54:38 +0300

    Refactor the code to have a single assert function

--- testing/pointers/pointers_test.go
+++ testing/pointers/pointers_test.go
@@ -3,27 +3,25 @@ package main
 import "testing"
 
 func TestWallet(t *testing.T) {
-	t.Run("deposit into wallet", func(t *testing.T) {
-		wallet := Wallet{}
-		wallet.Deposit(Bitcoin(10.0))
-
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
 		got := wallet.Balance()
-		want := Bitcoin(10.0)
 
 		if got != want {
 			t.Errorf("expected %s but got %s", want, got)
 		}
+	}
+
+	t.Run("deposit into wallet", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10.0))
+		want := Bitcoin(10.0)
+		assertBalance(t, wallet, want)
 	})
 
 	t.Run("with from wallet", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(30.0)}
-
 		wallet.Withdraw(Bitcoin(10))
-		got := wallet.Balance()
 		want := Bitcoin(20.0)
-
-		if got != want {
-			t.Errorf("expected %s but got %s", want, got)
-		}
+		assertBalance(t, wallet, want)
 	})
 }
