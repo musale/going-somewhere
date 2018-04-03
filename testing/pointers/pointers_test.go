f3f0ddb33613087253dcba55efd1b871ea6c015f
Martin Musale <martinmshale@gmail.com>
2018-04-03 19:52:30 +0300

    Add test for withdrawing from wallet

--- testing/pointers/pointers_test.go
+++ testing/pointers/pointers_test.go
@@ -3,13 +3,27 @@ package main
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10.0))
+	t.Run("deposit into wallet", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10.0))
 
-	got := wallet.Balance()
-	want := Bitcoin(10.0)
+		got := wallet.Balance()
+		want := Bitcoin(10.0)
 
-	if got != want {
-		t.Errorf("expected %s but got %s", want, got)
-	}
+		if got != want {
+			t.Errorf("expected %s but got %s", want, got)
+		}
+	})
+
+	t.Run("with from wallet", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(30.0)}
+
+		wallet.Withdraw(Bitcoin(10))
+		got := wallet.Balance()
+		want := Bitcoin(20.0)
+
+		if got != want {
+			t.Errorf("expected %s but got %s", want, got)
+		}
+	})
 }
